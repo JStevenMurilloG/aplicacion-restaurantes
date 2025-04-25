404f957ad5892a160561348bbc66954da6b5f412
JStevenMurilloG <stevennewps4@gmail.com>
2025-04-25 17:31:19 -0500

    versión inicial del proyecto

---
+++ src/main/java/co/proyectos/gestortareas/gestorrestaurantes/GestorRestaurantesApplication.java
@@ -0,0 +1,13 @@
+package co.proyectos.gestortareas.gestorrestaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorRestaurantesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorRestaurantesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/proyectos/gestortareas/gestorrestaurantes/GestorRestaurantesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.proyectos.gestortareas.gestorrestaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorRestaurantesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
